2de0e278df9895d74dd0b833ae6b2d50dd8f53d8
Rick van Dijk <rickavdijk@gmail.com>
2018-06-01 23:30:29 +0200

    Form and classes set-up

---
+++ BeeHive/BeeHive/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace BeeHive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.workerBeeJob = new System.Windows.Forms.ComboBox();
+            this.shifts = new System.Windows.Forms.NumericUpDown();
+            this.assignJobButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nextShift = new System.Windows.Forms.Button();
+            this.report = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Job";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(234, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Shifts";
+            // 
+            // workerBeeJob
+            // 
+            this.workerBeeJob.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.workerBeeJob.FormattingEnabled = true;
+            this.workerBeeJob.Items.AddRange(new object[] {
+            "Nectar collector",
+            "Honey manufacturing",
+            "Egg care",
+            "Baby bee tutoring",
+            "Hive maintenance",
+            "Sting patrol"});
+            this.workerBeeJob.Location = new System.Drawing.Point(39, 65);
+            this.workerBeeJob.Name = "workerBeeJob";
+            this.workerBeeJob.Size = new System.Drawing.Size(121, 21);
+            this.workerBeeJob.TabIndex = 2;
+            // 
+            // shifts
+            // 
+            this.shifts.Location = new System.Drawing.Point(237, 65);
+            this.shifts.Name = "shifts";
+            this.shifts.Size = new System.Drawing.Size(120, 20);
+            this.shifts.TabIndex = 3;
+            // 
+            // assignJobButton
+            // 
+            this.assignJobButton.Location = new System.Drawing.Point(39, 93);
+            this.assignJobButton.Name = "assignJobButton";
+            this.assignJobButton.Size = new System.Drawing.Size(318, 23);
+            this.assignJobButton.TabIndex = 4;
+            this.assignJobButton.Text = "Assign this job to a bee";
+            this.assignJobButton.UseVisualStyleBackColor = true;
+            this.assignJobButton.Click += new System.EventHandler(this.assignJobButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(34, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(328, 122);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Worker Bee Assignments";
+            // 
+            // nextShift
+            // 
+            this.nextShift.Location = new System.Drawing.Point(386, 16);
+            this.nextShift.Name = "nextShift";
+            this.nextShift.Size = new System.Drawing.Size(158, 122);
+            this.nextShift.TabIndex = 6;
+            this.nextShift.Text = "button1";
+            this.nextShift.UseVisualStyleBackColor = true;
+            this.nextShift.Click += new System.EventHandler(this.nextShift_Click);
+            // 
+            // report
+            // 
+            this.report.Location = new System.Drawing.Point(34, 158);
+            this.report.Multiline = true;
+            this.report.Name = "report";
+            this.report.Size = new System.Drawing.Size(510, 20);
+            this.report.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 450);
+            this.Controls.Add(this.report);
+            this.Controls.Add(this.nextShift);
+            this.Controls.Add(this.assignJobButton);
+            this.Controls.Add(this.shifts);
+            this.Controls.Add(this.workerBeeJob);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.shifts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox workerBeeJob;
+        private System.Windows.Forms.NumericUpDown shifts;
+        private System.Windows.Forms.Button assignJobButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button nextShift;
+        private System.Windows.Forms.TextBox report;
+    }
+}
+
